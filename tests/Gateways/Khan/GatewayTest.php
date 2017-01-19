b36867acd604d456468488d51bb55823744ddfac
selmonal <selmonal@gmail.com>
2017-01-19 14:18:45 +0800

    Implement guzzlehttp/client

--- tests/Gateways/Khan/GatewayTest.php
+++ tests/Gateways/Khan/GatewayTest.php
@@ -1,20 +1,35 @@
 <?php
 
-namespace Selmonal\Payways\Gateways\Khan;
-
-use Selmonal\Payways\TestCase;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
+use Selmonal\Payways\Gateways\Khan\Gateway;
 
 class GatewayTest extends TestCase
 {
     public function setUp()
     {
         parent::setUp();
-        $this->gateway = new Gateway($this->getHttpClient());
+
+        $this->gateway = new Gateway;
+    }
+
+    public function setHttpResponse($code, $body)
+    {
+        $mock = new MockHandler([
+            new Response($code, [], $body)
+        ]);
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+
+        $this->gateway->setHttpClient($client);
     }
 
     public function testProcessSuccess()
     {
-        $this->setMockHttpResponse('SuccessProcess.txt');
+        $this->setHttpResponse(200, '{"errorCode":"0","orderId":"560bc033-3ce8-40f5-a6e6-23e9b742df98","formUrl":"https://epp.khanbank.com/payment/merchants/q2ozbx/payment_en.html?mdOrder=560bc033-3ce8-40f5-a6e6-23e9b742df98"}');
+
         $response = $this->gateway->sendProcess($this->makeTransaction());
 
         $this->assertFalse($response->isSuccessful());
@@ -28,13 +43,13 @@ class GatewayTest extends TestCase
      */
     public function testProcessFailed()
     {
-        $this->setMockHttpResponse('FailedProcess.txt');
+        $this->setHttpResponse(200, '{"errorCode":"5","errorMessage":"Access denied"}');
         $this->gateway->sendProcess($this->makeTransaction());
     }
 
     public function testCompleteProcessSuccess()
     {
-        $this->setMockHttpResponse('SuccessCompleteProcess.txt');
+        $this->setHttpResponse(200, '{"expiration":"201610","cardholderName":"TEST TEST","depositAmount":0,"currency":"496","authCode":2,"ErrorCode":"0","ErrorMessage":"Success","OrderStatus":2,"OrderNumber":"356","Pan":"555555**5555","Amount":9691,"Ip":"202.170.71.91"}');
         $transaction = $this->makeTransaction();
         $transaction->reference = '560bc033-3ce8-40f5-a6e6-23e9b742df98';
         $response = $this->gateway->sendCompleteProcess($transaction);
@@ -47,7 +62,7 @@ class GatewayTest extends TestCase
 
     public function testCompleteProcessFailed()
     {
-        $this->setMockHttpResponse('FailedCompleteProcess.txt');
+        $this->setHttpResponse(200, '{"expiration":"201610","cardholderName":"TEST TEST","depositAmount":0,"currency":"496","authCode":2,"ErrorCode":"2","ErrorMessage":"Payment is declined","OrderStatus":6,"OrderNumber":"356","Pan":"555555**5555","Amount":9691,"Ip":"202.170.71.91"}');
         $transaction = $this->makeTransaction();
         $transaction->reference = '560bc033-3ce8-40f5-a6e6-23e9b742df98';
         $response = $this->gateway->sendCompleteProcess($transaction);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,11 +1,19 @@
 <?php
 
-namespace Selmonal\Payways;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use Selmonal\Payways\Transaction;
 
-use Omnipay\Tests\TestCase as OmniTestCase;
-
-class TestCase extends OmniTestCase
+class TestCase extends Orchestra\Testbench\TestCase
 {
+	/**
+	 * Make a new transaction instance.
+	 * 
+	 * @param  integer $id      
+	 * @param  integer $amount  
+	 * @param  string  $currency
+	 * @return Transacation
+	 */
     protected function makeTransaction($id = 100, $amount = 7300, $currency = 'MNT')
     {
         return new Transaction(compact('id', 'amount', 'currency'));
